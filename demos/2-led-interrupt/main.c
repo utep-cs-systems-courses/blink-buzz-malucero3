4e8012c7f2157705e708fcfc6cddc023b7031b39
malucero3 <malucero3@miners.utep.edu>
2022-10-19 11:27:31 -0600

    worked on demos 1 and 2

--- demos/2-led-interrupt/main.c
+++ demos/2-led-interrupt/main.c
@@ -3,6 +3,8 @@
 #include "libTimer.h"
 #include "led.h"
 
+void change_led();
+
 int main(void) {
   P1DIR |= LEDS;
   P1OUT &= ~LED_GREEN;
@@ -14,10 +16,34 @@ int main(void) {
   or_sr(0x18);		/* CPU off, GIE on */
 }
 
+static char led_state = 0;
+static char nCalls = 0;
 
-void
-__interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
+void __interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
 {
-  P1OUT |= LED_GREEN;
+  "once we reach interrupt 250 in a second we reset and go to change_led"
+  if (nCalls >= 250){
+    nCalls = 0;
+    change_led();
+  }
+  nCalls++;
 } 
 
+void change_led()
+{
+  "after a second we enter the switch initially start with 0, turn on the light and change to a 1"
+  switch (led_state) {
+  case (0):
+    P1OUT |= LED_GREEN; "actually red light"
+    led_state = 1;
+    break;
+  case(1):
+    P1OUT &= ~LED_GREEN; "after another second come here and turn off the light" 
+    led_state = 0;
+    break;
+  default:
+    led_state = 0;
+    break;
+    "This continues to alternate infinitely unless button is pushed (resets)"
+  }
+}
